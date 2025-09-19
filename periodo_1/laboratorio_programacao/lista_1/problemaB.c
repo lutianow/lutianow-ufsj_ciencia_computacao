8fdb980e83f7545cf126bc573f7ddb5bda6b0c84
Luciano Júnior <luciano.jr@sh3.com.br>
2025-09-19 15:22:31 -0300

    Resolução exercício B.

---
+++ periodo_1/laboratorio_programacao/lista_1/problemaB.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float x;
+    printf("Escreva um valor real para x para ser calculado na função f(x) = 3x² - 2x + 7:\n");
+    scanf("%f", &x);
+    x = (3 * x * x) - (2 * x) + 7;
+    printf("A imagem desse x é: %.2f\n", x);
+    return 0;
+}
