08aabcb822ce894bc52bb65587a51780c4fe4e9e
luciano.jr <lucianojdsonline@gmail.com>
2025-10-18 15:55:09 -0300

    Indentação.

--- periodo_1/laboratorio_programacao/lista_5/problemaB.c
+++ periodo_1/laboratorio_programacao/lista_5/problemaB.c
@@ -1,25 +1,25 @@
 
 #include <stdio.h>
-    void eliminaNegativos(float v[], int t){
-    for(int i = 0; i < t; i++)
-        if (2 * v[i] - 10 < 0)
-            v[i] = 0;  
+void eliminaNegativos(float v[], int t){
+for(int i = 0; i < t; i++)
+    if (2 * v[i] - 10 < 0)
+        v[i] = 0;  
+}
+
+int main(){
+    const int t = 2;
+    //scanf("%d", &t);
+    float v[t];
+
+    for(int i = 0; i < t; i++){
+        scanf("%f", &v[i]);
     }
 
-    int main(){
-        const int t = 2;
-        //scanf("%d", &t);
-        float v[t];
-
-        for(int i = 0; i < t; i++){
-            scanf("%f", &v[i]);
-        }
-
-        eliminaNegativos(v, t);
-            
-        for(int i = 0; i < t; i++){
-            printf("%.2f\n", v[i]);
-        }
-
-        return 0;
+    eliminaNegativos(v, t);
+        
+    for(int i = 0; i < t; i++){
+        printf("%.2f\n", v[i]);
     }
+
+    return 0;
+}
